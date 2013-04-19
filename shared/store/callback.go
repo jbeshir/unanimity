5399aadcd989b4bb393f5d83c212c5dbdf67ba30
John R Beshir <john@beshir.org>
2013-04-19 04:58:25 +0100

    Add stubs of callback-adding functionality to shared/store.
    Callbacks are never actually called yet.

---
+++ shared/store/callback.go
@@ -0,0 +1,60 @@
+package store
+
+var chosenCallbacks []func(slot uint64)
+var appliedCallbacks []func(slot uint64, idempotentChanges []Change)
+var proposalCallbacks []func()
+var degradedCallbacks []func()
+
+// Adds a callback to be called when a new instruction value is chosen.
+// Called with the slot number of the chosen instruction value,
+// after it has been added.
+//
+// The callback must not attempt to start a transaction,
+// or mutate values held by the store package,
+// and may assume no other changes have occurred since the change
+// the callback corresponds to.
+//
+// May only be called prior to calling Startup(), usually from init().
+func AddChosenCallback(cb func(slot uint64)) {
+	chosenCallbacks = append(chosenCallbacks, cb)
+}
+
+// Adds a callback to be called when an instruction value is applied.
+// Called with the slot number of the applied instruction value
+// and an idempotent version of that instruction value’s changeset,
+// as applied, after the instruction value is applied.
+//
+// The callback must not attempt to start a transaction,
+// or mutate values held by the store package,
+// and may assume no other changes have occurred since the change
+// the callback corresponds to.
+//
+// May only be called prior to calling Startup(), usually from init().
+func AddAppliedCallback(cb func(slot uint64, idempotentChanges []Change)) {
+	appliedCallbacks = append(appliedCallbacks, cb)
+}
+
+// Adds a callback to be called when the current proposal number or
+// leader node ID changes.
+//
+// The callback must not attempt to start a transaction,
+// or mutate values held by the store package,
+// and may assume no other changes have occurred since the change
+// the callback corresponds to.
+//
+// May only be called prior to calling Startup(), usually from init().
+func AddProposalCallback(cb func()) {
+	proposalCallbacks = append(proposalCallbacks, cb)
+}
+
+// Adds a callback to be called when the degraded status of the state changes.
+//
+// The callback must not attempt to start a transaction,
+// or mutate values held by the store package,
+// and may assume no other changes have occurred since the change
+// the callback corresponds to.
+//
+// May only be called prior to calling Startup(), usually from init().
+func AddDegradedCallback(cb func()) {
+	degradedCallbacks = append(degradedCallbacks, cb)
+}
