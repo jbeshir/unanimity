d82e949c48dd2c284aaf70f0a4d1526d13f6bb23
John R Beshir <john@beshir.org>
2013-05-02 17:13:36 +0100

    shared/connect: Have the recipient of connections authenticate them.
    This is because the client authentication doesn't work right now.
    This makes tests insecure, but that's not critical.

--- shared/connect/dial.go
+++ shared/connect/dial.go
@@ -16,6 +16,8 @@ import (
 // Panics if it is called with protocol set to CLIENT_PROTOCOL.
 func Dial(protocol int, id uint16) (*BaseConn, error) {
 
+	log.Print("dialing node ", id)
+
 	if protocol == CLIENT_PROTOCOL {
 		panic("tried to make outgoing client protocol connection")
 	}
@@ -29,8 +31,14 @@ func Dial(protocol int, id uint16) (*BaseConn, error) {
 	tlsConfig.Certificates = []tls.Certificate{*config.Certificate()}
 	tlsConfig.RootCAs = config.NodeCertPool(id)
 
+	// We rely on the receiving node to do TLS authentication for now.
+	// This is safe because it verifies our identity for us.
+	// Backwards to the usual arrangement but should be secure.
+	tlsConfig.InsecureSkipVerify = true
+
 	tlsConn, err := tls.Dial("tcp", ipStr+":"+portStr, tlsConfig)
 	if err != nil {
+		log.Print(err)
 		return nil, err
 	}
 
@@ -110,6 +118,33 @@ func Listen(protocol int, handler func(id uint16, b *BaseConn)) {
 		}
 	}
 }
+
+// Makes an outgoing connection to the given node using the client protocol.
+// Only used for test clients.
+// Returns a non-nil error if it is unable to connect.
+func TestDial(id uint16) (*BaseConn, error) {
+
+	ip := config.NodeIP(id)
+	ipStr := ip.String()
+	port := getProtocolPort(CLIENT_PROTOCOL)
+	portStr := strconv.FormatInt(int64(port), 10)
+
+	tlsConfig := new(tls.Config)
+	tlsConfig.RootCAs = config.NodeCertPool(id)
+
+	// TODO: Client-side verification of the server in tests.
+	// Clients cannot rely on the receiving end of the connection
+	// authenticating them.
+	tlsConfig.InsecureSkipVerify = true
+
+	tlsConn, err := tls.Dial("tcp", ipStr+":"+portStr, tlsConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return newBaseConn(tlsConn), nil
+}
+
 
 // Return the port we use for the given protocol.
 func getProtocolPort(protocol int) int {
